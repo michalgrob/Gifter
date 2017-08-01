3d72d5c9490c46e740b78c70f91842c05948b866
Sapirv <sapirv@gmail.com>
2017-08-01 22:06:50 +0300

    button chart icone interest michal function

--- routes/models/interest.js
+++ routes/models/interest.js
@@ -9,6 +9,7 @@ var Schema = mongoose.Schema;
 var interestSchema = new Schema({
     name: String,
     id: Number,
+    category:Number
 
 });
 
@@ -27,6 +28,74 @@ interestSchema.pre('save', function(next) {
     });
 
 });
+interestSchema.methods.addCategory= function addCategory()  {// =
+    switch (this.name)
+    {
+        case "Arts":
+        case "Music":
+        case "Movies":
+        case"Technology and Innovation":
+        case "Singing":
+        case "Dance":
+        case "Drawing":
+        case "Science Fiction":
+        case "Filming":
+        case "Religion and Theology":
+        case "Education":
+            this.category= 1;//Leisure
+            break;
+        case "Travel":
+        case "Cycling":
+        case "Camping & Hiking":
+        case "Extreme":
+        case "Swimming":
+        case "Fishing":
+        case "Basketball":
+        case "Running":
+        case "Fitness & Body Building":
+        case "Skiing & Snowboarding":
+        case "Soccer" :
+        case "Surfing" :
+            this.category=2; //Sports & Outdoors
+            break;
+        case "Jewelleries":
+        case "Clothes":
+        case "Shoes":
+        case "Accessories":
+        case "Optics":
+        case "Makeup":
+            this.category=3; //Fashion
+            break;
+        case "Office":
+        case "Do It Yourself":
+        case "Pets":
+        case "Electronics":
+        case "Gadgets":
+        case "Cooking":
+            this.category=4;
+            break; //Home & Garden
+        case "Reading":
+        case "Collecting":
+        case "Magics":
+        case "Origami":
+        case "Board Games":
+        case "Video Games":
+        case "Toys":
+            this.category=5;//Indoor Hobbies
+            break;
+        case "Night Life":
+        case "Smoking":
+        case "Cultivation":
+        case "Yoga":
+        case "Automobiles":
+        case "Alcohol":
+        case "Food Culture":
+            this.category=6;//Life Style
+            break;
+
+
+    }
+};
 
 var Interest = mongoose.model('interest', interestSchema);
 module.exports = Interest;
