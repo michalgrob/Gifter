8d341b101344874d1b3b35c1e7ac5df53b7ee4a3
Sapirv <sapirv@gmail.com>
2017-08-12 18:19:49 +0300

    ss

--- routes/models/Client.js
+++ routes/models/Client.js
@@ -3,15 +3,20 @@
  */
 var mongoose = require('mongoose');
 var User = require('./User');
+var Gift=require('./Gift');
 
 // Define client as a discriminator of user
-var clientSchema = User.discriminator('client', new mongoose.Schema(
-    {
+var clientSchema = User.discriminator('client', new mongoose.Schema({
         age: Number,
         interests: [String],
         events:{type: mongoose.Schema.Types.ObjectId, ref: 'Event'},
         gender: String,
-    }));
+        shoppingCart:[{type: mongoose.Schema.Types.ObjectId, ref: 'Gift'}]
+
+    // return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+
+}));
+
 
 // create the model for users and expose it to our app
 module.exports = clientSchema;
