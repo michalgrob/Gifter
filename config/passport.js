0371395eb1eec4192b93344686eda68a52ba77cf
Yoav Feldman <yoavfe1@gmail.com>
2017-08-31 19:24:00 +0300

    Yoav: Change Header

--- config/passport.js
+++ config/passport.js
@@ -107,4 +107,38 @@ module.exports = function(passport) {
         }));
 
 
+    //Yoav 05.08.17:
+
+    passport.use('create-store-manager', new LocalStrategy({
+            usernameField: 'email',
+            passwordField: 'password',
+            passReqToCallback: true
+        },
+
+        function(req, email, password, done) {
+            process.nextTick(function() {
+                User.findOne({ email:  email }, function(err, user) {
+                    if (err)
+                        return done(err);
+                    if (user) {
+                        return done(null, false, req.flash('signupMessage', 'That email is already in use.'));
+                    } else {
+                        var storeManager = new StoreManager();
+                        storeManager .email = email;
+                        storeManager .password = storeManager.generateHash(password);
+                        storeManager .name= req.body.name;
+                        storeManager .username= req.body.username;
+                        storeManager .admin= false;
+                        storeManager.store_id=req.body.store_id;
+
+                        //Save into DB:
+                        storeManager.save(function(err) {
+                            if (err)
+                                throw err;
+                            return done(null, storeManager);
+                        });
+                    }
+                });
+            });
+        }));
 };
